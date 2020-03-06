a784215be3d9b1df4d4179161aa222310abb1577
vivisuke <ntsuda@master.email.ne.jp>
2020-03-07 07:48:52 +0900

    HIerBufer クラス をとりあえず追加

---
+++ vivi/buffer/HierBuffer.h
@@ -0,0 +1,50 @@
+﻿//----------------------------------------------------------------------
+//
+//			File:			"HierBuffer.h"
+//			Created:		06-3ar-2020
+//			Author:			津田伸秀
+//			Description:	階層的データ構造のテキストバッファクラス
+//
+//----------------------------------------------------------------------
+
+#pragma once
+
+#include "gap_buffer.h"
+
+template<typename Type>
+class HierBuffer {
+public:
+	typedef Type value_type;
+#ifdef		_WIN64
+	typedef __int64 size_type;
+	typedef __int64 ssize_t;
+	typedef __int64 difference_type;	//	ギャップが無いとみなした場合のインデックス差
+	typedef __int64 index_type;			//	バッファインデックス [0, size()]
+	typedef __int64 pos_t;					//	バッファインデックス [0, size()]
+#else
+	typedef __int32 size_type;
+	typedef __int32 ssize_t;
+	typedef __int32 difference_type;	//	ギャップが無いとみなした場合のインデックス差
+	typedef __int32 index_type;			//	バッファインデックス [0, size()]
+	typedef __int32 pos_t;					//	バッファインデックス [0, size()]
+#endif
+	typedef Type& reference;
+	typedef const Type& const_reference;
+	typedef Type* pointer;
+	typedef const Type* const_pointer;
+public:
+	HierBuffer()
+		: m_size(0)
+		, m_curPage(0)
+	{
+	}
+public:
+	bool isEmpty() const { return !m_size; }
+	bool empty() const { return !m_size; }
+	size_type size() const { return m_size; }
+private:
+	gap_buffer<gap_buffer<Type>>	m_buffer;
+	size_type	m_size;		//	データトータルサイズ
+	int	m_curPage;		//	現ページ、ランダムアクセス時は現ページから優先して探索
+	
+};
--- vivi/testBuffer/testBuffer.cpp
+++ vivi/testBuffer/testBuffer.cpp
@@ -1,6 +1,37 @@
 ﻿#include <iostream>
+#include <assert.h>
+#include "../buffer/gap_buffer.h"
+#include "../buffer/HierBuffer.h"
+
+void test_gap_buffer();
+void test_HierBuffer();
 
 int main()
 {
+	test_gap_buffer();
+	//
     std::cout << "OK\n";
+}
+void test_gap_buffer()
+{
+	gap_buffer<char> gb;
+	assert( gb.isEmpty() );
+	assert( gb.empty() );
+	assert( gb.size() == 0 );
+	//
+	gb.insert(0, 'a');
+	assert( !gb.isEmpty() );
+	assert( gb.size() == 1 );
+	assert( gb[0] == 'a' );
+	gb.insert(0, 'x');
+	assert( gb.size() == 2 );
+	assert( gb[0] == 'x' );
+	assert( gb[1] == 'a' );
+}
+void test_HierBuffer()
+{
+	HierBuffer<char> gb;
+	assert( gb.isEmpty() );
+	assert( gb.empty() );
+	assert( gb.size() == 0 );
 }
