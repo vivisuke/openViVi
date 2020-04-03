d171e2634cdedc5030e946d54c0ab8e40936fcb4
vivisuke <ntsuda@master.email.ne.jp>
2020-04-04 06:21:15 +0900

    ◎ C 04/04 04/04 0.001 ToDo 起動時：「無題」タブをひとつだけ生成 ◎ C 04/04 04/04 0.001 ToDo File > New：Tab 追加

--- vivi/vivi64/MainWindow.cpp
+++ vivi/vivi64/MainWindow.cpp
@@ -1,10 +1,40 @@
 #include "MainWindow.h"
+//#include "EditView.h"
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
 	: QMainWindow(parent)
+	, m_curTabIndex(-1)
+	, m_formerTabIndex(-1)
+	, m_docNumber(0)
 {
 	ui.setupUi(this);
-	char *ptr = nullptr;
-	qDebug() << "sizeof(ptr) = " << sizeof(ptr) << "\n";
+	//char *ptr = nullptr;
+	//qDebug() << "sizeof(ptr) = " << sizeof(ptr) << "\n";
+	connectMenuActions();
+	
+	//	デザイナでタブの消し方がわからないので、ここで消しておく
+	while( ui.tabWidget->count() )
+		ui.tabWidget->removeTab(0);
+	//
+		on_action_New_triggered();
+}
+void MainWindow::connectMenuActions()
+{
+	//QObject::connect(ui.action_New, SIGNAL(triggered()), this, SLOT(on_action_New_triggered()));
+}
+void MainWindow::on_action_New_triggered()
+{
+	qDebug() << "on_action_Open_triggered()\n";
+	EditView* view = new QPlainTextEdit();	//createView();
+	QString title = tr("Untitled-%1").arg(++m_docNumber);
+	addNewView(view, title);
+}
+void MainWindow::addNewView(EditView *view, const QString &title)
+{
+		ui.tabWidget->addTab(view, title);
+}
+void MainWindow::on_action_Open_triggered()
+{
+	qDebug() << "on_action_Open_triggered()\n";
 }
--- vivi/vivi64/MainWindow.h
+++ vivi/vivi64/MainWindow.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+#include <QPlainTextEdit>
 #include "ui_MainWindow.h"
+
+typedef QPlainTextEdit	EditView;
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +13,19 @@ class MainWindow : public QMainWindow
 public:
 	MainWindow(QWidget *parent = Q_NULLPTR);
 
+protected:
+	void	connectMenuActions();
+	//EditView	*createView(Document *doc = 0, TypeSettings* = 0);
+	void	addNewView(EditView *, const QString &title);
+
+private slots:
+	void	on_action_New_triggered();
+	void	on_action_Open_triggered();
+	
 private:
 	Ui::MainWindowClass ui;
+	
+	int		m_curTabIndex;
+	int		m_formerTabIndex;
+	int		m_docNumber;
 };
