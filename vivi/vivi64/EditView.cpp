81a57961096aac46310f3b7126127eb35cc88158
vivisuke <ntsuda@master.email.ne.jp>
2020-05-16 14:27:07 +0900

    0506 ◎ C 05/16 05/16 0.015│問題│v コマンドでクラッシュする

--- vivi/vivi64/EditView.cpp
+++ vivi/vivi64/EditView.cpp
@@ -1461,7 +1461,7 @@ int EditView::textWidth(pos_t first, ssize_t sz, /*pos_t last,*/ const Buffer* p
 	auto pos = first;
 	int col = 0;
 	while( pos != endpos ) {
-		auto ch = pbuffer->operator[](pos);
+		auto ch = pos < pbuffer->size() ? pbuffer->operator[](pos) : '\0';
 		if( ch == '\t' ) {
 			auto n = (nTab - col % nTab);
 			col += n;
